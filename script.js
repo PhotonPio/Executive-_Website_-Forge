e686536450ce18ab5005dacf13e0fd9cbdff2c74
PhotonPio <166168368+PhotonPio@users.noreply.github.com>
2026-03-04 22:27:44 -0800

    Create script.js with Alpine.js application code for siteApp

---
+++ script.js
@@ -0,0 +1,11 @@
+function siteApp() {
+    return {
+        // Define your data properties here
+        title: 'My Website',
+        description: 'A brief description of the website',
+        items: [], // Your items array or any other data you need
+        init() {
+            // Initialization logic, such as fetching data or setting default values
+        }
+    };
+}
